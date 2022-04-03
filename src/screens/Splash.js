48ee35ade5e176bf3ca67aad7d3904ba2ad2f69a
haseebnk <haseebnk37@gmail.com>
2022-04-04 04:58:00 +0500

    splash wrok

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState , useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   StyleSheet,
   Image,
@@ -10,7 +10,7 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
   Switch,
-  ScrollView,LayoutAnimation,
+  ScrollView, LayoutAnimation,
   Platform,
   UIManager,
   Animated
@@ -19,21 +19,22 @@ import LinearGradient from 'react-native-linear-gradient';
 // import SplashScreen from 'react-native-splash-screen';
 import * as Animatable from 'react-native-animatable';
 import { transform } from 'lodash';
-
+import { moderateScale } from 'react-native-size-matters';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
-      UIManager.setLayoutAnimationEnabledExperimental(true);
+    UIManager.setLayoutAnimationEnabledExperimental(true);
   }
 }
 
 export default function Splashing(props) {
 
-  
+
 
   useEffect(() => {
-    setTimeout(() => {fadeIn() }, 1000)
-    setTimeout(() => props.navigation.navigate("slider") , 2000);
+    setTimeout(() => { fadeIn() }, 1000)
+    setTimeout(() => { fadeInUp() }, 1000)
+    // setTimeout(() => props.navigation.navigate("slider") , 2000);
   }, []);
 
   const fadeAnim = useRef(new Animated.Value(-130)).current;
@@ -41,20 +42,22 @@ export default function Splashing(props) {
     // Will change fadeAnim value to 1 in 5 seconds
     Animated.timing(fadeAnim, {
       toValue: 1,
-      duration: 1000
-  
+      duration: 1000,
+      useNativeDriver:true
+
     }).start();
   };
 
   const fadeAnimUp = useRef(new Animated.Value(130)).current;
- 
- 
+
+
   const fadeInUp = () => {
     // Will change fadeAnim value to 1 in 5 seconds
     Animated.timing(fadeAnimUp, {
-      toValue: 1,
-      duration: 1500
-  
+      toValue: -1,
+      duration: 1000,
+      useNativeDriver:true
+
     }).start();
   };
 
@@ -63,28 +66,32 @@ export default function Splashing(props) {
   return (
     <TouchableWithoutFeedback >
       <View style={styles.container}>
-      
-      
-      
 
-      <Animated.View   style={{height:180,  translateY:fadeAnim}}>
-        <Animatable.Text delay={1000} style={styles.welcome}  animation="slideInDown" iterationCount={1} direction="alternate">welcome to</Animatable.Text>
-        <Animatable.Text delay={1000} style={styles.datenight} animation="slideInDown" iterationCount={1} direction="alternate">Date Night</Animatable.Text>
-        <Animatable.Text delay={1000} style={styles.datingText}  animation="slideInDown" iterationCount={1} direction="alternate">Dating just got fun again!</Animatable.Text>
+
+
+
+        <Animated.View style={{ translateY: fadeAnim }}>
+          <Animatable.Text delay={1000} style={styles.welcome} animation="slideInDown"  >welcome to</Animatable.Text>
+          <Animatable.Text delay={1000} style={styles.datenight} animation="slideInDown">Date Night</Animatable.Text>
+          <Animatable.Text delay={1000} style={styles.datingText} animation="slideInDown" >Dating just got fun again!</Animatable.Text>
         </Animated.View>
-        <Animatable.Image   style={styles.img}  animation="zoomIn"  source={require('../assets/imglogo.png')}
-        >
-          {/* <Image
-            style={styles.tinyLogo}
-            source={require('./src/imglogo.png')}
-          /> */}
+
+
+
+        <Animatable.Image style={styles.img} animation="zoomIn" source={require('../assets/imglogo.png')} >
+
         </Animatable.Image>
-        <Animated.View   style={{height:100,  translateY:fadeAnimUp}}>
-        <Animatable.Text style={styles.getStarted} animation="slideInUp" iterationCount={1} direction="alternate">Get started in just</Animatable.Text>
-        <Animatable.Text  style={styles.steps} animation="slideInUp"  iterationCount={1} direction="alternate"><Text style={{ color: 'red',
-    fontSize: 25,
-    fontFamily: 'Poppins',}}>4</Text> simple steps...</Animatable.Text>
-     </Animated.View>
+
+
+
+        <Animated.View style={{ translateY: fadeAnimUp }}>
+          <Animatable.Text style={styles.getStarted} animation="slideInUp" >Get started in just</Animatable.Text>
+          <Animatable.Text style={styles.steps} animation="slideInUp"  ><Animatable.Text animation="slideInUp" style={{
+            color: 'red',
+            fontSize: 25,
+            fontFamily: 'Poppins',
+          }}>4</Animatable.Text> simple steps...</Animatable.Text>
+        </Animated.View>
       </View>
 
     </TouchableWithoutFeedback>
@@ -94,73 +101,75 @@ export default function Splashing(props) {
 
 const styles = StyleSheet.create({
   img: {
-    width: 150,
-    height: 150,
-    marginTop: 100,
-    marginBottom:100,
+    width: moderateScale(170),
+    height: moderateScale(170),
+    marginTop: moderateScale(180),
+    marginBottom: moderateScale(180),
     alignSelf: "center",
-    resizeMode:'contain'
+    resizeMode: 'contain'
 
   },
   steps: {
     color: 'white',
-    fontSize: 23,
+    fontSize: 20,
     fontFamily: 'Poppins',
 
     // marginTop: 180,
     // marginBottom: 10,
     alignSelf: "center",
     alignContent: "center",
-    
+
 
   },
   getStarted: {
     color: 'white',
     fontFamily: 'Poppins',
-    fontSize: 23,
-   
+    fontSize: 20,
+
     // marginBottom: -230,
     alignSelf: "center",
-  
+
 
   },
   container: {
     flex: 1,
     backgroundColor: "black",
-    paddingBottom:40,
-   
+    paddingBottom:moderateScale(10),
+    paddingTop:moderateScale(10),
+
+
 
   },
   welcome: {
     color: 'white',
-    fontFamily: "Poppins-Regular", 
+    fontFamily: "Poppins-Regular",
 
-    fontSize: 23,
+    fontSize: 20,
 
     alignSelf: "center",
     alignContent: "center",
-   
+
 
   },
   datenight: {
     color: '#FF2B25',
-    fontSize: 63,
-    
-    fontFamily: "Poppins-Bold", 
-   
+    fontSize: 53,
+
+    fontFamily: "Poppins-Bold",
+
     alignSelf: "center",
     alignContent: "center",
-   
+
 
   },
   datingText: {
     color: '#A5A5A5',
-    fontSize: 23,
-    fontFamily: "Poppins", 
-    
+    fontSize: 20,
+    fontFamily: "Poppins",
+
     alignSelf: "center",
     alignContent: "center",
-    
+
 
   }
 
